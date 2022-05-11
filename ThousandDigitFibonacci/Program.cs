4185af2be1f2280872ce351824cb62bfc8648ae1
Low-SkillCoder <IshaanAdhikary@outlook.com>
2022-05-10 21:54:22 -0400

    Fixed Fibonacci Digits by not trying too hard late at night

--- ThousandDigitFibonacci/Program.cs
+++ ThousandDigitFibonacci/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 
 namespace ThousandDigitFibonacci
 {
@@ -6,32 +7,20 @@ namespace ThousandDigitFibonacci
     {
         static void Main(string[] args)
         {
-            int answer = GetFibonacciWithDigits(1000);
+            int answer = GetFibonacciIndexWithDigits(1000);
             Console.WriteLine(answer);
             Console.ReadKey();
         }
 
-        static int GetFibonacciWithDigits(int length) {
-            int finalIndex;
-            int a = 1;
-            int aPow = 0;
-            int b = 1;
-            int bPow = 0;
-            for (finalIndex = 3; true; finalIndex++) {
-                if (a.ToString().Length > 7) {
-                    aPow += a.ToString().Length - 7;
-                    a = Int32.Parse(a.ToString().Substring(0, 7));
-                }
-                if (b.ToString().Length > 7) {
-                    bPow += b.ToString().Length - 7;
-                    b = Int32.Parse(b.ToString().Substring(0, 7));
-                }
-                int temp = b;
+        static int GetFibonacciIndexWithDigits(int length) {
+            int finalIndex = 2;
+            BigInteger a = 1;
+            BigInteger b = 1;
+            while (b.ToString().Length < length) {
+                BigInteger temp = b;
                 b += a;
-                if (b.ToString().Length + bPow >= 1000){
-                    break;
-                }
                 a = temp;
+                finalIndex++;
             }
             return finalIndex;
         }
